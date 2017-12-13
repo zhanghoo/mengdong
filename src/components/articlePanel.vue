18743fc1e92fa2216fc577ba0a7d77aabd7ba367
Hoo <zhanghoo@163.com>
2017-12-14 07:42:51 +0800

    大致框架结构搭建

---
+++ src/components/articlePanel.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="article-panel"></div>
+</template>
+
+<script>
+export default {
+	name: 'article-panel'
+}
+</script>
+
+<style lang="scss">
+.article-panel {
+}	
+</style>
