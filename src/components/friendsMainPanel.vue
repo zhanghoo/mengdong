8d6590ae79fc01cafd9412c3a88ca78347fb548e
Hoo <zhanghoo@163.com>
2017-12-14 14:18:58 +0800

    大致框架结构搭建

---
+++ src/components/friendsMainPanel.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="friends-main-panel">
+    <template v-for="active in friendsList">
+      <template v-if="active.type == 0">
+        <friends-article-panel :article="active"></friends-article-panel>
+      </template>
+      <template v-else-if="active.type == 1">
+        <friends-image-panel :image="active"></friends-image-panel>
+      </template>
+      <template v-else>
+        <friends-video-panel :video="active"></friends-video-panel>
+      </template>
+    </template>
+  </div>
+</template>
+
+<script>
+import friendsArticlePanel from '@/component/friendsArticlePanel'
+import friendsImagePanel from '@/component/friendsImagePanel'
+import friendsVideoPanel from '@/component/friendsVideoPanel'
+
+export default {
+  name: 'friends-main-panel',
+  components: {
+    friendsArticlePanel,
+    friendsImagePanel,
+    friendsVideoPanel
+  },
+  props: {
+    friendsList: Object
+  }
+}
+</script>
+
+<style lang="scss">
+.friends-main-panel {
+
+}
+</style>
