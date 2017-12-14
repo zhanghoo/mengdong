629c1feab98e198ed31ec9abe121c22d98c240ca
Hoo <zhanghoo@163.com>
2017-12-14 16:43:46 +0800

    Create appThirdLogin.vue

---
+++ src/components/appThirdLogin.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="app-third-login">
+    <p class="title-line">或者</p>
+    <p class="third-login">
+      <span class="icon icon-qq"></span>
+      <span class="icon icon-wechat"></span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'app-third-login'
+}
+</script>
+
+<style lang="scss">
+.app-third-login {
+}
+</style>
