266397b0b2968382fff7b5f3d19210f9a44690ab
Hoo <zhanghoo@163.com>
2017-12-15 07:49:42 +0800

    Update register.vue

--- src/components/register.vue
+++ src/components/register.vue
@@ -2,6 +2,7 @@
 	<div class="register">
     <div class="bg"></div>
     <div class="wrap">
+      <span class="icon icon-return"></span>
       <div class="input-block">
         <span class="icon icon-phone"></span>
         <input type="text" placeholder="请输入手机号">
