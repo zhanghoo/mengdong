45837a9c59ea71b48ae6b97417771488266ac91b
Hoo <zhanghoo@163.com>
2017-12-14 11:18:07 +0800

    大致框架结构搭建

--- src/components/imagePanel.vue
+++ src/components/imagePanel.vue
@@ -1,5 +1,22 @@
 <template>
-	<div class="image-panel"></div>
+	<div class="image-panel">
+    <div class="cover">
+      <img src="">
+      <span class="img-num"></span>
+    </div>
+    <div class="author">
+      <div class="avatar"></div>
+      <div class="name">暴走的短腿猫</div>
+    </div>
+    <div class="content">他们说，猫是不恋家的，最后会决绝离开。</div>
+    <div class="info">
+      <span class="date">今天</span>
+      <p class="count">
+        <span class="icon icon-collection"><i class="num"></i></span>
+        <span class="icon icon-share"><i class="num"></i></span>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
