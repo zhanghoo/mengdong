2a6e4d5f8ca17472f3662749e8348e209620e3fc
Hoo <zhanghoo@163.com>
2017-12-14 16:11:09 +0800

    Update aboutMainPanel.vue

--- src/components/aboutMainPanel.vue
+++ src/components/aboutMainPanel.vue
@@ -1,14 +1,9 @@
 <template>
 	<div class="about-main-panel">
-    <div class="header">
-      <div class="head-bar"></div>
-      <div class="bg"></div>
-      <div class="about">
-        <div class="avatar"></div>
-        <p class="name"><span class="text">丫丫<span></p>
-        <p class="signature">当你善待它的时候，它会以意想不到的方式回报你</p>
-      </div>
-    </div>
+    <home-header>
+      <div slot="left">左</div>
+      <div slot="right"></div>
+    </home-header>
     <main-panel></main-panel>
     <div class="footer">
       <a href="javascrip:;" class="btn-chat">聊天有趣的</a>
@@ -17,10 +12,12 @@
 </template>
 
 <script>
+import homeHeader from '@/component/homeHeader'
 import mainPanel from '@/component/mainPanel'
 export default {
 	name: 'about-main-panel',
   components: {
+    homeHeader,
     mainPanel
   }
 }
