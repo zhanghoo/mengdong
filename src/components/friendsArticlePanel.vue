ae7bed011f4e484662287c13759e6aceddb940e2
Hoo <zhanghoo@163.com>
2017-12-14 14:58:12 +0800

    Update friendsArticlePanel.vue

--- src/components/friendsArticlePanel.vue
+++ src/components/friendsArticlePanel.vue
@@ -1,11 +1,24 @@
 <template>
 	<div class="friend-article-panel">
+    <friends-slot-panel>
+      <div class="article-content">
+        <div class="img"></div>
+        <div class="cover">
+          <h2 class="title-line">美文</h2>
+          <h3 class="title">有生命就会有灵魂，不管它是不是人类</h3>
+        </div>
+      </div>
+    </friends-slot-panel>
   </div>
 </template>
 
 <script>
+import friendsSlotPanel from '@/component/friendsSlotPanel'
 export default {
-	name: 'friend-article-panel'
+	name: 'friend-article-panel',
+  components: {
+    friendsSlotPanel
+  }
 }
 </script>
 
