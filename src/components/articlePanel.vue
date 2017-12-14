fec825006e1f830b756ff229ca3bb94cbdc4c0e6
Hoo <zhanghoo@163.com>
2017-12-14 11:01:38 +0800

    大致框架结构搭建

--- src/components/articlePanel.vue
+++ src/components/articlePanel.vue
@@ -1,5 +1,17 @@
 <template>
-	<div class="article-panel"></div>
+	<div class="article-panel">
+    <p class="text-line">ONE STORY</p>
+    <h2 class="title">如果我是一个人，或许会写一封诗意盎然的告别信，然后潇洒地离开。</h2>
+    <div class="cover"></div>
+    <div class="contnet">如果转换成人类的语言，这将是一封告别信</div>
+    <div class="footer">
+      <span class="date">今天</span>
+      <p class="count">
+        <span class="icon icon-collection"><i class="num"></i></span>
+        <span class="icon icon-share"><i class="num"></i></span>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
