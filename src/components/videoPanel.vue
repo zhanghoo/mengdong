709839cfd71466dcd057c174f609e6f8038aac56
Hoo <zhanghoo@163.com>
2017-12-14 11:32:22 +0800

    大致框架结构搭建

--- src/components/videoPanel.vue
+++ src/components/videoPanel.vue
@@ -1,5 +1,27 @@
 <template>
-	<div class="video-panel"></div>
+	<div class="video-panel">
+    <div class="author">
+      <div class="avatar"></div>
+      <div class="user">
+        <p class="name"><span class="text">咪咪酱</span></p>
+        <p class="tag"><span class="text">萌宠奴</span></p>
+      </div>
+      <div class="follow">
+        <span>加为宠伴</span>
+        <span>我的宠伴</span>
+      </div>
+    </div>
+    <div class="content">
+      <div class="cover"></div>
+    </div>
+    <div class="footer">
+      <span class="date">今天</span>
+      <p class="count">
+        <span class="icon icon-collection"><i class="num"></i></span>
+        <span class="icon icon-share"><i class="num"></i></span>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
