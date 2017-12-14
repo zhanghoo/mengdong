754176324f968a1589d252b019d201b3a65585eb
Hoo <zhanghoo@163.com>
2017-12-15 07:46:00 +0800

    Update login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,31 @@
 <template>
-	<div class="login"></div>
+	<div class="login">
+    <div class="bg"></div>
+    <div class="wrap">
+      <div class="input-block">
+        <span class="icon icon-phone"></span>
+        <input type="text" placeholder="手机号">
+        <span class="icon icon-close"></span>
+      </div>
+      <div class="input-block">
+        <span class="icon icon-password"></span>
+        <input type="password" placeholder="密码">
+        <span class="icon icon-close"></span>
+      </div>
+      <a href="javascript:;" class="btn btn-main btn-register">登录</a>
+      <a href="javascript:;" class="">忘记密码</a>
+    </div>
+    <app-third-login></app-third-login>
+  </div>
 </template>
 
 <script>
+import appThirdLogin from '@/components/appThirdLogin'
 export default {
-  name: 'login'
+  name: 'login',
+  components: {
+    appThirdLogin
+  }
 }
 </script>
 
