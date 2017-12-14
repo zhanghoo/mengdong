5b7ad51c355e5d1b3ad914d9bbad1bfc5dc06f2e
Hoo <zhanghoo@163.com>
2017-12-14 14:41:55 +0800

    Create friendsArticlePanel.vue

---
+++ src/components/friendsArticlePanel.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="friend-article-panel">
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'friend-article-panel'
+}
+</script>
+
+<style lang="scss">
+.friend-article-panel {
+}
+</style>
