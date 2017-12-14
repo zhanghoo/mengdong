4cdff9c99af1e27a6619c07cd29373d24ed8f737
Hoo <zhanghoo@163.com>
2017-12-15 07:40:55 +0800

    Update register.vue

--- src/components/register.vue
+++ src/components/register.vue
@@ -1,10 +1,36 @@
 <template>
-	<div class="register"></div>
+	<div class="register">
+    <div class="bg"></div>
+    <div class="wrap">
+      <div class="input-block">
+        <span class="icon icon-phone"></span>
+        <input type="text">
+        <span class="icon icon-close"></span>
+      </div>
+      <div class="input-block">
+        <span class="icon icon-password"></span>
+        <input type="password">
+        <span class="icon icon-close"></span>
+      </div>
+      <div class="input-block">
+        <span class="icon icon-verify"></span>
+        <input type="text">
+        <span class="icon icon-close"></span>
+        <a href="javascript:;" class="btn btn-small btn-get-verify">获取</a>
+      </div>
+      <a href="javascript:;" class="btn btn-main btn-register">注册</a>
+    </div>
+    <app-third-login></app-third-login>
+  </div>
 </template>
 
 <script>
+import appThirdLogin from '@/components/appThirdLogin'
 export default {
-  name: 'register'
+  name: 'register',
+  components: {
+    appThirdLogin
+  }
 }
 </script>
 
