0f806c0529f63e6096462e2fa63bcc48aa63b28d
Hoo <zhanghoo@163.com>
2017-12-15 07:47:12 +0800

    Update register.vue

--- src/components/register.vue
+++ src/components/register.vue
@@ -4,17 +4,17 @@
     <div class="wrap">
       <div class="input-block">
         <span class="icon icon-phone"></span>
-        <input type="text">
+        <input type="text" placeholder="请输入手机号">
         <span class="icon icon-close"></span>
       </div>
       <div class="input-block">
         <span class="icon icon-password"></span>
-        <input type="password">
+        <input type="password" placeholder="设置登录密码，不少于6位">
         <span class="icon icon-close"></span>
       </div>
       <div class="input-block">
         <span class="icon icon-verify"></span>
-        <input type="text">
+        <input type="text" placeholder="输入验证码">
         <span class="icon icon-close"></span>
         <a href="javascript:;" class="btn btn-small btn-get-verify">获取</a>
       </div>
