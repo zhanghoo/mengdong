7bf1f6d56e45159c362c7bbbe435f34a2be1efb9
Hoo <zhanghoo@163.com>
2017-12-15 15:00:21 +0800

    Update appMessagePage.vue

--- src/components/appMessagePage.vue
+++ src/components/appMessagePage.vue
@@ -1,11 +1,23 @@
 <template>
-  <div v-show="showFlag" class="app-message-page"></div>
+  <div v-show="showFlag" class="app-message-page">
+    <app-header>
+      <span slot="left" class="icon icon-back" @click="hide"></span>
+      <span slot="title">{{msgFrom}}</span>
+      <template slot="right"></template>
+    </app-header>
+    <div class="app-chat-wrap">
+      <ul class="app-chat">
+        <li v-for="msg in msges" class="app-chat-item" :class="[msg.type == 0 ? item-right : item-left]"></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'app-message-page',
   props: {
+    msgFrom: String,
     msges: Array
   },
   data () {
