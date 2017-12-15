26f8fd51b131ff2fcd72a95ce5ee33b4dfab1688
Hoo <zhanghoo@163.com>
2017-12-15 08:43:38 +0800

    Update logreg.vue

--- src/components/logreg.vue
+++ src/components/logreg.vue
@@ -3,18 +3,16 @@
     <div class="bg"></div>
     <div class="wrap">
       <h1 class="title">嘿！就剩你了，快上车！</h1>
-      <slot>
-      <a href="javascript:;" class="btn btn-main btn-login">登录</a>
-      <a href="javascript:;" class="btn btn-default">注册</a>
-      </slot>
+      <a href="javascript:;" class="btn btn-main btn-login" @click="showLogin">登录</a>
+      <a href="javascript:;" class="btn btn-default" @click="showReg">注册</a>
     </div>
     <app-third-login></app-third-login>
-    <login>
+    <login v-show="loginShowFlag">
       <div slot="operation">
         <span class="icon icon-back" @click="hideLogin"></span>
       </div>
     </login>
-    <register>
+    <register v-show="regShowFlag">
       <div slot="operation">
         <span class="icon icon-back" @click="hideReg"></span>
       </div>
@@ -32,6 +30,26 @@ export default {
     appThirdLogin,
     login,
     register
+  },
+  data () {
+    return {
+      loginShowFlag: false,
+      regShowFlag: false
+    }
+  },
+  methods: {
+    showLogin () {
+      this.showLogin = true
+    },
+    hideLogin () {
+      this.showLogin = false
+    },
+    showReg () {
+      this.regShowFlag = true
+    },
+    hideReg () {
+      this.regShowFlag = false
+    }
   }
 }
 </script>
