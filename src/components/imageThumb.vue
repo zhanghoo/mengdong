e79844f121e3933cfa02367356eaffd60602ef35
Hoo <zhanghoo@163.com>
2017-12-15 09:42:12 +0800

    Create imageThumb.vue

---
+++ src/components/imageThumb.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="image-thumb">
+    <ul class="images clearfix">
+      <li class="img-item"></li>
+      <li class="img-item"></li>
+      <li class="img-item"></li>
+      <li class="img-item"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'image-thumb'
+}
+</script>
+
+<style lang="scss">
+.image-thumb {
+  .images {
+    position: relative;
+    .img-item {
+      float: left;
+      padding-bottom: 100%;
+      width: 45%;
+      heightL 0;
+      background: #ccc;
+    }
+  }
+}
+</style>
