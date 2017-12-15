d728833276f0b6a14e47ebcf22d8f9458b8f386a
Hoo <zhanghoo@163.com>
2017-12-15 15:51:30 +0800

    Create shopMainPanel.vue

---
+++ src/components/shopMainPanel.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="shop-main-panel"></div>
+</template>
+
+<script>
+export default {
+  name: 'shop-main-panel'
+}
+</script>
+
+<style lang="scss">
+.shop-main-panel {
+}
+</style>
