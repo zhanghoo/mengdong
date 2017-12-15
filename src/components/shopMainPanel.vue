91d1a333cbd2ffd3168f99cf1d1e22e4a2cffef2
Hoo <zhanghoo@163.com>
2017-12-15 17:00:41 +0800

    Update shopMainPanel.vue

--- src/components/shopMainPanel.vue
+++ src/components/shopMainPanel.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="shop-main-panel"></div>
+  <div class="shop-main-panel">
+    <div class="shop-nav">
+      <a href="javascript:;" class="shop-nav-item">精选</a>
+      <a href="javascript:;" class="shop-nav-item">热销</a>
+      <a href="javascript:;" class="shop-nav-item">现实</a>
+    </div>
+    <div class="shop-content"></div>
+  </div>
 </template>
 
 <script>
