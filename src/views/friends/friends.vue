031505db36a4f2a5eed07e66bbe517752b4e5884
Hoo <zhanghoo@163.com>
2017-12-15 11:08:51 +0800

    Update friends.vue

--- src/views/friends/friends.vue
+++ src/views/friends/friends.vue
@@ -4,23 +4,26 @@
       <span slot="left"></span>
       <span slot="title">伴</span>
       <template slot="right">
-        <span class="icon icon-search"></span>
+        <span class="icon icon-message" @click="show"></span>
       </template>
     </app-header>
     <friends-main-panel :friends-list="friendsList"></friends-main-panel>
+    <app-message-page ref="message"></app-message-page>
   </div>
 </template>
 
 <script>
 import appHeader from '@/components/appHeader'
 import friendsMainPanel from '@/components/friendsMainPanel'
+import appMessagePage from '@/components/appMessagePage'
 import axios from 'axios'
 
 export default {
   name: 'friends',
   components: {
     appHeader,
-    friendsMainPanel
+    friendsMainPanel,
+    appMessagePage
   },
   data () {
     return {
@@ -31,6 +34,11 @@ export default {
     axios.get('static/mocks/friends/data.json').then((res) => {
       this.friendsList = res.data
     })
+  },
+  methods: {
+    show () {
+      this.$refs.message.show();
+    }
   }
 }
 </script>
