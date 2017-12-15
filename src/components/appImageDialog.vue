764e390bb5219605ac1c68cffed649f690403f1c
Hoo <zhanghoo@163.com>
2017-12-15 10:57:46 +0800

    Update appImageDialog.vue

--- src/components/appImageDialog.vue
+++ src/components/appImageDialog.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="image-dialog">
+	<div v-show="showFlag" class="image-dialog">
     <div class="content">
       <div class="image-wrapper"></div>
       <div class="text">我今年九岁，用你们人类的年龄计算，意识耄耋之年，如果我是一个人，或许会写一封诗意盎然的告别信。然后潇洒地离开。但是我只是一直猫，我只能用我的呼噜声与你告别。</div>
@@ -14,7 +14,17 @@
 
 <script>
 export default {
-  name: 'image-dialog'
+  name: 'image-dialog',
+  data () {
+    return {
+      showFlag: false
+    }
+  },
+  methods: {
+    show () {
+      this.showFlag = true
+    }
+  }
 }
 </script>
 
