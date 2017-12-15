7046918eab74a0e269a7b5b99fdf8ce95ee6dada
Hoo <zhanghoo@163.com>
2017-12-15 14:32:31 +0800

    Create appMessagePage.vue

---
+++ src/components/appMessagePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-show="showFlag" class="app-message-page"></div>
+</template>
+
+<script>
+export default {
+  name: 'app-message-page',
+  props: {
+    msges: Array
+  },
+  data () {
+    showFlag: false
+  },
+  methods: {
+    show () {
+      this.showFlag = true
+    },
+    hide () {
+      this.showFlag = false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.app-message-page {
+}
+</style>
