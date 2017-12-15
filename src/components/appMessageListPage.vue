3694ae33ec64ef9d4afa7cf8d4ac8d9f75a4a1bc
Hoo <zhanghoo@163.com>
2017-12-15 14:24:58 +0800

    Create appMessageListPage.vue

---
+++ src/components/appMessageListPage.vue
@@ -0,0 +1,66 @@
+<template>
+  <div v-show="showFlag" class="app-message-list-page">
+    <app-header>
+      <span slot="left" @click="hide"></span>
+      <span slot="title">消息中心</span>
+      <template slot="right">
+        <span class="icon icon-search" @click="show"></span>
+      </template>
+    </app-header>
+    <div class="content">
+      <ul class="app-msg-list">
+        <li class="app-msg-item" v-for="msg in msgList" :key="msg.id" @click="showMsgPage(msg)"></li>
+      </ul>
+    </div>
+    <app-message-page :msg="msges" ref="messagePage"></app-message-page>
+  </div>
+</template>
+
+<script>
+import appHeader from '@/components/appHeader'
+import appMessagePage from '@/components/appMessagePage'
+import axios from 'axios'
+
+export default {
+  name: 'app-message-list-page',
+  components: {
+    appHeader,
+    appMessagePage
+  },
+  data () {
+    return {
+      showFlag: false,
+      msgList: [],
+      msges: []
+    }
+  }
+  created () {
+    axios.get('static/mocks/data.json').then((res) => {
+      this.msgList = res.data
+    })
+  },
+  methods: {
+    show () {
+      this.showFlag = true
+    },
+    hide () {
+      this.showFlag = false
+    },
+    showMsgPage (msges) {
+      this.msges = msges
+      this.$refs.messagePage.show()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.app-message-list-page {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: $zIndexPage;
+}
+</style>
