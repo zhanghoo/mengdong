6319a8eba8bff6aa196e0d8bd06254d34da43d1f
Hoo <zhanghoo@163.com>
2017-12-18 11:46:04 +0800

    Create shopOrderPage.vue

---
+++ src/components/shopOrderPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="shop-order-page" v-show="showFlag"></div>
+</template>
+
+<script>
+export default {
+  name: 'shop-order-page',
+  data () {
+    return () {
+      showFlag: false
+    }
+  },
+  methods: {
+    show () {
+      this.showFlag = true
+    },
+    hide () {
+      this.showFlag = false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.shop-order-page {
+}
+</style>
