8fe26b12cf630d9c7f8ea43c89613502d81c4237
Hoo <zhanghoo@163.com>
2017-12-18 15:22:57 +0800

    Create shopTextsPage.vue

---
+++ src/components/shopTextsPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="shop-texts-page"></div>
+</template>
+
+<script>
+export default {
+  name: 'shop-texts-page'
+}
+</script>
+
+<style lang="scss">
+.shop-texts-page {} 
+</style>
