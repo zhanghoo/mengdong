1f0754a6f37872b8e61637c0ef9a71ec0d116010
Hoo <zhanghoo@163.com>
2017-12-18 08:52:37 +0800

    Update shop.vue

--- src/views/shop/shop.vue
+++ src/views/shop/shop.vue
@@ -4,22 +4,36 @@
       <span slot="left"></span>
       <span slot="title">遛</span>
       <template slot="right">
-        <span class="icon icon-type"></span>
-        <span class="icon icon-cart"></span>
+        <span class="icon icon-type" @click="showType"></span>
+        <span class="icon icon-cart" @click="showCart"></span>
       </template>
     </app-header>
     <shop-main-panel></shop-main-panel>
+    <shop-pet-type-page href="shopPetTypePage"></shop-pet-type-page>
+    <shop-cart-page href="shopCartPage"></shop-cart-page>
   </div>
 </template>
 
 <script>
 import appHeader from '@/components/appHeader'
 import shopMainPanel from '@/components/shopMainPanel'
+import shopPetTypePage from '@/components/shopPetTypePage'
+import shopCartPage from '@/components/shopCartPage'
 export default {
   name: 'shop',
   components: {
     appHeader,
-    shopMainPanel
+    shopMainPanel,
+    shopPetTypePage,
+    shopCartPage
+  },
+  methods: {
+    showType () {
+      this.$refs.shopPetTypePage.show()
+    },
+    showCart () {
+      this.$refs.shopCartPage.show()
+    }
   }
 }
 </script>
