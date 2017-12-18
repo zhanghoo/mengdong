b24c3d316c7518e1c83a6e0ad23ca2d4970deb26
Hoo <zhanghoo@163.com>
2017-12-18 11:43:19 +0800

    Create shopGoodPage.vue

---
+++ src/components/shopGoodPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="shop-good" v-show="showFlag">
+     <app-header>
+      <span slot="left" class="icon icon-back"></span>
+      <span slot="title">商品页</span>
+      <span slot="right" class="icon icon-share"></span>
+    </app-header>
+  </div>
+</template>
+
+<script>
+import appHeader from '@/components/appHeader'
+export default {
+  name: 'shop-good',
+  components: {
+    appHeader
+  },
+  data () {
+    return {
+      showFlag: false
+    }
+  },
+  methods: {
+    show () {
+      this.showFlag = true
+    },
+    hide () {
+      this.showFlag = false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.shop-good {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #ccc;
+}
+</style>
